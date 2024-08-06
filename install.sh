57d742a20232a4004bcd155b0ae76739394a9ab6
Victor Gauthereau <vgauther@student.42.fr>
2024-08-06 14:16:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-java -jar forge-installer.jar --installServer
+wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.3.0/forge-1.20.1-47.3.0-installer.jar ~/mc_forge_1_20_6/forge-1.20.1-47.3.0-installer.jar
+java -jar ~/mc_forge_1_20_6/forge-1.20.1-47.3.0-installer.jar --installServer
